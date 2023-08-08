6bb7e14bbf8a727e4609aaf1ddff549ddc78511b
ZRNRD <zrnrd@mail.ru>
2023-08-08 19:21:48 +0500

    Исправлено отображение кнопок меню

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -5,20 +5,21 @@ import style from './Menu.module.scss';
 
 export const Menu = ({changeHeader}) => {
     const [showMenu, isShowMenu] = React.useState(false);
+    const [activeButton, changeActiveButton] = React.useState('Таймер');
     return (
         <div className={style["menu"]}>
             <div className={showMenu ? style["menu__items-show"] : style["menu__items"]}>
                 <NavLink to="/timer">
                     <input type="button" 
-                        className={style["menu__item-active"]} 
+                        className={activeButton === 'Таймер' ? style["menu__item-active"] : style["menu__item"]} 
                         value="Таймер"
-                        onClick={()=>changeHeader("Таймер")}></input>
+                        onClick={()=>{changeHeader("Таймер"); changeActiveButton('Таймер')}}></input>
                 </NavLink>
-                <NavLink to="/stopwatch">
+                <NavLink to="/stopwatch" >
                     <input type="button" 
-                        className={style["menu__item"]} 
+                        className={activeButton === 'Секундомер' ? style["menu__item-active"] : style["menu__item"]} 
                         value="Секундомер"
-                        onClick={()=>changeHeader("Секундомер")}></input>
+                        onClick={()=>{changeHeader("Секундомер"); changeActiveButton('Секундомер')}}></input>
                 </NavLink>
             </div>
             <input type="button" className={style["menu-show"]} value="Меню" onClick={()=>{isShowMenu(!showMenu)}}></input>
